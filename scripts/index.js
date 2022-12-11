bf25b0a87f1579bb722e8e964d2e3c748c49ec0d
Pechinkin Sergey <pechinkinsergey@yandex.ru>
2022-12-12 02:04:57 +0600

    Fixed after first review

--- scripts/index.js
+++ scripts/index.js
@@ -64,34 +64,6 @@ const resetPopup = () => {
   linkInput.value = '';
 };
 
-//---------------------------------images-------------------------------------------
-const initialCards = [
-  {
-    name: 'Эверест',
-    link: 'https://images.unsplash.com/photo-1575819719798-83d97dd6949c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80'
-  },
-  {
-    name: 'Ачипсинские водопады',
-    link: '../images/Ачипсинские.jpg'
-  },
-  {
-    name: 'Гора Чогори',
-    link: 'https://images.unsplash.com/photo-1627896157734-4d7d4388f28b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-  },
-  {
-    name: 'Бзерпинский карниз',
-    link: '../images/Бзерпинский.jpg'
-  },
-  {
-    name: 'Гора Эльбрус',
-    link: 'https://images.unsplash.com/photo-1638989420853-a6437f7a0d2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80'
-  },
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-];
-
 //============================add card like delete viewPopup==========================================
 
 const createElement = (item) => {
@@ -121,7 +93,6 @@ const createElement = (item) => {
   btnClosePhoto.addEventListener('click', () => closePopup(popupPhoto));
 
   return elementItem;
- 
 };
 
 //========================================================================================
