38572c74f1309a76b992f2a2eb1d6c30356d16ae
Pechinkin Sergey <pechinkinsergey@yandex.ru>
2022-12-24 23:30:22 +0600

    wrong works popup

--- scripts/validate.js
+++ scripts/validate.js
@@ -6,14 +6,23 @@ const validationConfig = {
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
 }; 
+function resetValidation(formElement, inputElement, config) {
+  const error = formElement.querySelector(`#${inputElement.id}-error`);
 
+  inputElement.classList.remove(config.inputErrorClass);
+  error.classList.remove(config.errorClass);
+  error.textContent = '';
+};
+
+// ------------ Add error class---------------
 const showInputError = (formElement, inputElement, config) => {
   const error = formElement.querySelector(`#${inputElement.id}-error`);
 
   inputElement.classList.add(config.inputErrorClass);
   error.classList.add(config.errorClass);
-  error.textContent = inputElement.validationMessage; }
-
+  error.textContent = inputElement.validationMessage; 
+}
+// --------------- Hide error class -------------
 const hideInputError = (formElement, inputElement, config) => {
   const error = formElement.querySelector(`#${inputElement.id}-error`);
 
@@ -22,41 +31,77 @@ const hideInputError = (formElement, inputElement, config) => {
   error.textContent = '';
 }
 
-function validateInput(formElement, inputElement, config) {
+
+//------------ check validity and use add or remove error-class function---------
+function validateInput(formElement, inputElement, config) { 
   
 if(!inputElement.validity.valid) {
-
   showInputError(formElement, inputElement, config);
-
 } else {
-
-  hideInputError(formElement, inputElement, config);
-  
+  hideInputError(formElement, inputElement, config); 
   }
 }
 
-function setHandlers(formElement, config) {
-const inputList = Array.from(formElement.querySelectorAll(config.inputElement));
-console.log(inputList);
-inputList.forEach((inputElement) => {
-  inputElement.addEventListener('input', () => {
-   validateInput(formElement, inputElement, config);
-  })
-})
-}
 
+//---------Get inputList all forms-- and use validity function for Each-----------
+function setHandlers(formElement, config) {
+  const inputList = Array.from(formElement.querySelectorAll(config.inputElement));
+  const button = formElement.querySelector(config.submitButtonSelector);
+  
+  toggleButton(inputList, button, config);
+ 
+  inputList.forEach((inputElement) => {
+    
+    inputElement.addEventListener('input', () => {
+    toggleButton(inputList, button, config);
+    validateInput(formElement, inputElement, config);
+
+    toggleButton(inputList, button, config);
+  });
+  hideInputError(formElement, inputElement, config); 
+});
+
+};
+
+
+//const button = formElement.querySelectorAll(config.submitButtonSelector);
+//-----------button disabled off---------------------------
+  //const button = document.querySelector('.popup__button');
+// console.log(button);
+// button.disabled = false;
+//---------------------button----------------------------------
+
+const checkInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+
+const toggleButton = (inputList, button, config) => {
+  
+  if (checkInput(inputList)) {
+    button.classList.add(config.inactiveButtonClass);
+    button.disabled = true;
+  } else {
+    button.classList.remove(config.inactiveButtonClass);
+    button.disabled = false;
+  }
+};
+
+
+
+//--------Get all forms and use handlers function for each???------------------------
 function enableValidation(config) {
   const form = document.querySelectorAll(config.formElement);
-   console.log(form);
+   //console.log(form);
    form.forEach((formElement) => {
   setHandlers(formElement, config);
 });
 
   // form.addEventListener('submit', (evt) => {
-  //   evt.preventDefault(); 
+  // evt.preventDefault(); 
   // });
-
 }
-
 
 enableValidation(validationConfig);
